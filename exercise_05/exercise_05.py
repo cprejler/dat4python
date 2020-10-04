ccb4517f38ad90796a8d8e4ce7e80390e49123d7
cprejler <prejler93@gmail.com>
2020-10-04 19:10:26 +0200

    exercise 05

--- exercise_05/exercise_05.py
+++ exercise_05/exercise_05.py
@@ -1,6 +1,24 @@
 import pandas as pd 
-url = 'https://api.statbank.dk/v1/data/FOLK1A/JSONSTAT'
-dst = pd.read_json(url)
-dst.to_csv('dk-stat-all-tables.csv', encoding='utf-8', index=False)
+pd.options.mode.chained_assignment = None  # default='warn'
+divorced_df = pd.read_csv('divorced.csv', sep=';')
 
-print(dst.iloc[:,1])
+#What is the change in pct of divorced danes from 2008 to 2020?
+
+divorced2008 = divorced_df.iloc[0,2]
+divorced2020 = divorced_df.iloc[1,2]
+
+divorceChangeInPercent = (divorced2020-divorced2008) / divorced2008 * 100
+
+print(divorceChangeInPercent)
+
+#Which of the 5 biggest cities has the highest percentage of 'Never Married' in 2020?
+
+never_married_df = pd.read_csv('nevermarried.csv', sep=';')
+
+totalTop5 = never_married_df[(never_married_df['OMRÅDE'] != 'Hele landet') & (never_married_df['OMRÅDE'] != 'Region Hovedstaden') &(never_married_df['OMRÅDE'] != 'Region Midtjylland') &  (never_married_df['OMRÅDE'] != 'Region Syddanmark') & (never_married_df['OMRÅDE'] != 'Region Sjælland') & (never_married_df['OMRÅDE'] != 'Region Nordjylland')]
+
+
+
+totalTop5.sort_values('INDHOLD', ascending=False, inplace=True )
+print(totalTop5[:5])
+
